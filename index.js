e633d63e10aa0d4a0a8e3b2961785bf0afbdf182
li-arditi <liarditi@gmail.com>
2025-01-08 15:37:17 -0500

    changed layout and adding options

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function parseInput(textInput){
-    console.log(parseInput)
-
-
+function GetSearchParams(searchOptionsDiv){
+    var parameters = {};
+    // Go through search options and create parameters based on selections
+    $(searchOptionsDiv).children().each((idx,element) => {
+        var id = $(element).attr("id");
+        var value = $(element).val();
+        parameters[id] = value;
+    });
+    console.log(parameters)
+    //GetResults(parameters)
 }
+
